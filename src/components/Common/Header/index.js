fbbf5fa2b631427aab857b7fed174c566d7352a4
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-16 18:33:43 +0530

    Initial commit

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import TemporaryDrawer from './drawer';
+import './style.css'
+import Button from '../Button';
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className="navbar">
+      <h1 className="logo">
+        CryptoTracker<span style={{ color: "var(--blue)" }}>.</span>
+      </h1>
+      <div className="links">
+        <Link to="/">
+          <p className="link">Home</p>
+        </Link>
+
+        <Link to="/Compare">
+          <p className="link">Compare</p>
+        </Link>
+
+        <Link to="/Dashboard">
+          <Button
+            text={"Dashboard"}
+          
+            onClick={() => console.log("btncliked")}
+          />
+        </Link>
+      </div>
+      <div className="mobile-drawer">
+        <TemporaryDrawer />
+      </div>
+    </div>
+  );
+}
+export default Header;
