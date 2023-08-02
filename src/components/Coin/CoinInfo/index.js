f3538dd46f70734e343a347406b7414b7c9a2b65
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-08-02 21:51:40 +0530

    Initial commit

--- src/components/Coin/CoinInfo/index.js
+++ src/components/Coin/CoinInfo/index.js
@@ -1,32 +1,22 @@
-import React, { useState } from 'react';
-import './style.css';
-
+import React, { useState } from 'react'
+import "./style.css"
 function CoinInfo({ heading, desc }) {
-  const shortDesc = desc?.slice(0, 350) + '...';
-  const longDesc = desc;
-  const [showFullDesc, setShowFullDesc] = useState(false);
-
-  const toggleDescription = () => {
-    setShowFullDesc(!showFullDesc);
-  };
-
-  const renderDescription = () => {
-    return (
-      <div className="coin-info-description">
-        <p dangerouslySetInnerHTML={{ __html: showFullDesc ? longDesc : shortDesc }} />
-        {desc?.length > 350 && (
-          <button onClick={toggleDescription} className="read-more-button">
-            {showFullDesc ? 'Read Less' : 'Read More'}
-          </button>
-        )}
-      </div>
-    );
-  };
-
+  const shortDesc =
+    desc?.slice(0, 350) + "<p style='color:var(--gray)'> Read More...</p>";
+  const longDesc = desc + "<p style='color:var(--gray)'> Read Less...</p>";;
+  const[flag,setFlag]=useState(false)
   return (
-    <div className="grey-wrapper">
+    <div className="grey-wrapper" style={{ padding: "0rem 1rem" }}>
       <h2 className="coin-info-heading">{heading}</h2>
-      {desc?.length > 200 ? renderDescription() : <p dangerouslySetInnerHTML={{ __html: desc }} />}
+      {desc?.length > 200 ? (
+        <p
+          onClick={() => setFlag(!flag)}
+          className="coin-info-desc"
+          dangerouslySetInnerHTML={{ __html: !flag ? shortDesc : longDesc }}
+        />
+      ) : (
+        <p dangerouslySetInnerHTML={{ __html: desc }} />
+      )}
     </div>
   );
 }
